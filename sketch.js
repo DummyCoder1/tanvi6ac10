f4d481fe57c878b5d804de821fb3c65ee1384cf1
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-03-07 11:39:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var player;
+var platform;
+var player_image, platform_image;
+
+function preload(){
+  player_image=loadImage("images/jack2.png")
+  platform_image = loadImage("images/p1.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  player = createSprite(250,250,20,50);
+player.addImage(player_image)
+  platform = createSprite(250,350,100,10);
+  platform.addImage("platform", platform_image)
+  platform.scale=0.15
+}
+
+function draw() {
+  background('#F2F5B6');
+  if(keyDown("up")){
+    player.velocityY=-5
+
+  }
+  drawSprites();
+}
